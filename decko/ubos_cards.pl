7ebf3df4aa587bab44fe767235af524fd14be32b
Ethan McCutchen <ethan@grasscommons.org>
2018-02-26 16:05:43 -0500

    getting PKGBUILD and manifest closer

---
+++ decko/ubos_cards.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+#
+# Invoke the Rails db:migrate
+#
+# Copyright (C) 2017 and later, Indie Computing Corp. All rights reserved. License: see package.
+#
+
+use strict;
+use warnings;
+
+use UBOS::Logging;
+use UBOS::Utils;
+
+my $datadir = $config->getResolve( 'appconfig.datadir' );
+my $command;
+
+if ($operation eq 'install') {
+    $command = "decko seed";
+} elsif ($operation eq 'upgrade') {
+    $command = "decko update";
+}
+
+if ($command) {
+    my $out;
+    my $full_command = "cd $datadir/approot; bundle exec $command"
+    if( UBOS::Utils::myexec( $full_command, undef, \$out, \$out )) {
+        error( "$command failed:", $out );
+    }
+}
+
+1;
