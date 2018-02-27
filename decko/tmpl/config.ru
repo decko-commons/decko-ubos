8a4d6cd41df027e5d61bff94a468d0f234f79c6e
Ethan McCutchen <ethan@grasscommons.org>
2018-02-26 21:13:39 -0500

    ready to try a build

--- decko/tmpl/config.ru
+++ decko/tmpl/config.ru
@@ -2,4 +2,4 @@
 
 require ::File.expand_path('../config/environment',  __FILE__)
 
-run UBOSAppConfig::Application
+run DeckoUbos::Application
---
+++ decko/ubos_logging.pl
@@ -0,0 +1,30 @@
+
+#!/usr/bin/perl
+#
+# Sets up the directory / file structure for logging from a Rails app
+#
+# Copyright (C) 2017 and later, Indie Computing Corp. All rights reserved. License: see package.
+#
+
+use strict;
+use warnings;
+
+use UBOS::Logging;
+use UBOS::Utils;
+
+my $appconfigid = $config->getResolve( 'appconfig.appconfigid' );
+my $appid       = $config->getResolve( 'appconfig.appid' );
+
+my $logDir  = "/var/log/$appid/$appconfigid";
+my $logFile = "$logDir/production.log";
+
+if ($operation eq 'deploy') {
+    unless (-d $logDir) {
+        UBOS::Utils::mkdirDashP($logDir, 0755, 'http', 'http');
+    }
+    unless (-e $logFile) {
+        UBOS::Utils::saveFile($logFile, '', 0644, 'http', 'http');
+    }
+}
+
+1;
