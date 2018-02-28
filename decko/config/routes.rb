de15c145dd5ed97f2178e1b8df2632c144c78304
Ethan McCutchen <ethan@decko.org>
2018-02-28 19:40:09 +0000

    housecleaning, unused files, comments, etc

--- decko/config/routes.rb
+++ decko/config/routes.rb
@@ -1,7 +1,5 @@
 # -*- encoding : utf-8 -*-
 
 Decko.application.routes.draw do
-  rel_root = Cardio.config.relative_url_root
-
-  mount Decko::Engine => "#{rel_root}/"
+  mount Decko::Engine => "/"
 end
