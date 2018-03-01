ce4f8be92fcfd9ad4d7cf691c836281b1b390cee
Ethan McCutchen <ethan@grasscommons.org>
2018-03-01 15:38:08 -0500

    first go at webapptest

---
+++ decko/tests/DeckoTest.pm
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+#
+# Simple test for gladiwashere-php-mysql.
+#
+# Copyright (C) 2014 and later, Indie Computing Corp. All rights reserved. License: see package.
+#
+
+use strict;
+use warnings;
+
+package DeckoTest;
+
+use UBOS::WebAppTest;
+
+# The states and transitions for this test
+
+my $TEST = new UBOS::WebAppTest(
+    appToTest   => 'decko',
+    description => 'Tests whether decko app correctly loads',
+
+    # packageDbsToAdd => {
+    #    'toyapps' => 'http://depot.ubos.net/$channel/$arch/toyapps'
+    # },
+
+    checks => [
+            new UBOS::WebAppTest::StateCheck(
+                    name  => 'virgin',
+                    check => sub {
+                        my $c = shift;
+
+                        $c->getMustContain(    '/', '', undef, 'Wrong front page' );
+
+                        return 1;
+                    }
+            )
+    ]
+);
+
+# other tests to consider:
+# - creating initial user
+# - uploading a file
+# - js? css?
+
+
+$TEST;
