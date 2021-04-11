735a1c8019ec0e3ddefc90d15e5199eba73594de
Ethan McCutchen <ethan@decko.org>
2021-04-11 18:17:10 -0400

    setup tweaks

--- setup.sh
+++ setup.sh
@@ -4,13 +4,13 @@ ubos-admin update
 echo "installing dev packages needed for makepkg to work"
 pacman -S --noconfirm base-devel pacman-contrib decko
 
-echo "updating pacman config"
-cp /home/decko-ubos/decko/pacman.conf /etc/pacman.conf
-
-echo "setting up shepherd account"
-ubos-admin setup-shepherd
-passwd shepherd
-su shepherd
-
 echo "cd'ing to decko dir"
 cd /home/decko-ubos/decko
+
+echo "updating pacman config"
+cp pacman.conf /etc/pacman.conf
+
+echo "setting up shepherd account (password is 'shepherd')"
+ubos-admin setup-shepherd
+# echo "shepherd:shepherd" | chpasswd
+su shepherd
